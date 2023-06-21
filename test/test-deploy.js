fbe8b2590614a28aad1b62db4783c7a3323bfa50
MoeyJac <jo3ymac@gmail.com>
2023-06-21 16:07:15 -0400

    feat: added additional tests for 100% coverage and completeness

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -12,6 +12,7 @@ describe("SimpleStorage", function () {
         const expectedValue = "0"
         assert.equal(currentValue.toString(), expectedValue)
     })
+
     it("Should update when we call store", async function () {
         const expectedValue = "7"
         const transactionResponse = await simpleStorage.store(expectedValue)
@@ -20,5 +21,38 @@ describe("SimpleStorage", function () {
         const currentValue = await simpleStorage.retrieve()
 
         assert.equal(currentValue.toString(), expectedValue)
+    })
+
+    it("Should start with empty list of people", async function () {
+        const expectedLength = 0
+        const currentPeopleSize = await simpleStorage.getPeopleLength()
+
+        assert.equal(currentPeopleSize, expectedLength)
+    })
+
+    it("Should contain one person when adding to people", async function () {
+        const expectedLength = 1
+        const transactionResponse = await simpleStorage.addPerson("Joe", "14")
+        await transactionResponse.wait(1)
+
+        const currentPeopleSize = await simpleStorage.getPeopleLength()
+
+        assert.equal(currentPeopleSize, expectedLength)
+    })
+
+    it("Should return favorite number from person mapping", async function () {
+        const expectedNumber = "14"
+        const personsName = "Joe"
+        const transactionResponse = await simpleStorage.addPerson(
+            personsName,
+            expectedNumber
+        )
+        await transactionResponse.wait(1)
+
+        const personsFavoriteNumber = await simpleStorage.nameToFavoriteNumber(
+            personsName
+        )
+
+        assert.equal(personsFavoriteNumber, expectedNumber)
     })
 })
